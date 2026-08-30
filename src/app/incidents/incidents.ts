67f095c88c13e29db30a749fea6d4a803ab66acd
zanelem26-ha <zanelemabundam@gmail.com>
2026-08-30 11:00:53 +0200

    add-comment-details-to-incidents-page

--- src/app/incidents/incidents.ts
+++ src/app/incidents/incidents.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { IncidentsService } from '../services/incidents';
+import { IncidentCommentsService } from '../services/incident-comments';
 
 @Component({
   selector: 'app-incidents',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './incidents.html',
-  styleUrl: './incidents.css',
+  styleUrl: './incidents.css'
 })
+export class Incidents implements OnInit {
 
-export class Incidents {
-  
+  incidents: any[] = [];
+  expandedIncidentId: number | null = null;
+  comments: { [incidentId: number]: any[] } = {};
+
+  constructor(
+    private incidentsService: IncidentsService,
+    private incidentCommentsService: IncidentCommentsService
+  ) {}
+
+  ngOnInit(): void {
+
+    this.incidentsService.getIncidents()
+      .subscribe(data => {
+        this.incidents = data;
+      });
+
+  }
+
+  toggleIncident(incidentId: number): void {
+
+  if (this.expandedIncidentId === incidentId) {
+    this.expandedIncidentId = null;
+    return;
+  }
+
+  this.expandedIncidentId = incidentId;
+
+  this.incidentCommentsService
+    .getCommentsByIncident(incidentId)
+    .subscribe(data => {
+      this.comments[incidentId] = data;
+    });
+
+ }
 
 }
